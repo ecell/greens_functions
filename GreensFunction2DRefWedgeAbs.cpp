0c51258db992d5d72fc78680898c5e30c4e7376c
Toru Niina <niina.toru.68u@gmail.com>
2016-02-12 13:27:27 +0900

    fix: 2DRefWedgeAbs p_int_theta_2nd

--- GreensFunction2DRefWedgeAbs.cpp
+++ GreensFunction2DRefWedgeAbs.cpp
@@ -140,26 +140,34 @@ namespace greens_functions
     }
 
     const Real GreensFunction2DRefWedgeAbs::p_int_theta(const Real r,
-                                                const Real theta,
-                                                const Real t) const
+                                                        const Real theta,
+                                                        const Real t) const
     {
+        /* XXX: Theta definition isn't same as other GF. The initial position *
+         *      is not zero but phi/2. Compensation will be done by drawTheta.*
+         *      This function asserts the accepting value theta is in the     *
+         *      range [0, phi]                                                */
+
+        // where the r is zero, theta is not defined.
+        // return linearly incleasing function(integrated uniform distribution).
         if(fabs(r) < CUTOFF)
         {
-            return theta * 0.5 / M_PI;
+            return theta / this->phi;
         }
 
+        // when the initial r equals the abs boundary,
+        // particle cannot go anywhere.
         if(fabs(r-a) < CUTOFF)
         {
             return 0e0;
         }
 
-        if(theta == 0e0) return 0e0;
+        // this returns accumurate probability distribution,
+//         if(theta == phi) return 0e0; // theta == 0e0?
 //         if(fabs(theta - 2*M_PI) < CUTOFF) return 1e0;
 
         const Real first_term(p_int_theta_first(r, theta, t));
         const Real second_term(p_int_theta_second(r, theta, t));
-
-//         const Real denominator(p_int_2pi(r, t));
 
         return (first_term + second_term);
     }
@@ -223,9 +231,10 @@ namespace greens_functions
         return (2e0 * theta * sum / (this->phi * a * a));
     }
 
-    const Real GreensFunction2DRefWedgeAbs::p_int_theta_second(const Real r,
-                                                               const Real theta,
-                                                               const Real t) const
+    const Real
+        GreensFunction2DRefWedgeAbs::p_int_theta_second(const Real r,
+                                                        const Real theta,
+                                                        const Real t) const
     {
         const Real r_0(this->getr0());
         const Real a(this->geta());
@@ -238,19 +247,28 @@ namespace greens_functions
         Real sum(0e0);
         Real term(0e0);
 
-        // calculating
-        // 4 / (phi * a^2) * 
-        // sum_n [
-        //      phi / (n * pi) * sin(n pi theta / phi) * cos(n * pi / 2) * 
-        //      sum_m[
-        //           exp(-alpha_mn^2 * Dt) *
-        //           4 * J_{n * pi / phi}(r0 * alpha_mn) * J_{n * pi / phi}(r * alpha_mn) / 
-        //           (J_{n * pi / phi - 1}(a * alpha_mn) - J_{n * pi / phi + 1}(a * alpha_mn))^2 
-        //           ]
-        //       ]
-        //
+        // prepair (2pi / phi) and (2pi theta / phi). using tau = 2pi.
+        const Real tau_phi = 2e0 * M_PI / this->phi;
+//         const Real tau_theta_phi = tau_phi * theta;
+
+        // for (-1)^n
+        Real sgn = -1e0;
+
+        // calculating 
+        // 8 / (pi * a^2) * 
+        // sum_n^inf [
+        //     ((-1)^n / n) * sin(n * 2 * pi * theta / phi) * 
+        //     sum_m [
+        //         exp(-alpha_mn^2 * Dt) *
+        //         J_{n * 2pi / phi}(r0 * alpha_mn) *
+        //         J_{n * 2pi / phi}(r * alpha_mn) / 
+        //        (J_{n * 2pi / phi - 1}(a * alpha_mn) -
+        //         J_{n * 2pi / phi + 1}(a * alpha_mn))^2 
+        //     ]
+        // ]
+
         Integer n(1);
-        for(; n < num_out_term_use; ++n)
+        for(/*unsigned int n = 1*/; n < num_out_term_use; ++n)
         {
             Real in_sum(0e0);
             Real in_term(0e0);
@@ -265,17 +283,21 @@ namespace greens_functions
             Real Jnpp_d_1_a_alpha_mn(0e0);// J_n-1(a alpha_mn)
             Real Jnpp_p_1_a_alpha_mn(0e0);// J_n+1(a alpha_mn)
 
-            Real bessel_order(n * M_PI / this->phi);
-            Integer m(1);
+            Real bessel_order(n * tau_phi);
 
-            for(; m < num_in_term_use; ++m)
+            Integer m(1);
+            for(/*unsigned int m = 1*/; m < num_in_term_use; ++m)
             {
                 a_alpha_mn = gsl_sf_bessel_zero_Jnu(bessel_order, m);
                 alpha_mn = a_alpha_mn / a;
-                Jnpp_r_alpha_mn     = gsl_sf_bessel_Jnu(bessel_order, r * alpha_mn);
-                Jnpp_r0_alpha_mn    = gsl_sf_bessel_Jnu(bessel_order, r_0 * alpha_mn);
-                Jnpp_d_1_a_alpha_mn = gsl_sf_bessel_Jnu(bessel_order - 1, a_alpha_mn);
-                Jnpp_p_1_a_alpha_mn = gsl_sf_bessel_Jnu(bessel_order + 1, a_alpha_mn);
+                Jnpp_r_alpha_mn
+                    = gsl_sf_bessel_Jnu(bessel_order, r * alpha_mn);
+                Jnpp_r0_alpha_mn
+                    = gsl_sf_bessel_Jnu(bessel_order, r_0 * alpha_mn);
+                Jnpp_d_1_a_alpha_mn
+                    = gsl_sf_bessel_Jnu(bessel_order - 1, a_alpha_mn);
+                Jnpp_p_1_a_alpha_mn
+                    = gsl_sf_bessel_Jnu(bessel_order + 1, a_alpha_mn);
 
                 in_term1 = std::exp(alpha_mn * alpha_mn * minusDt);
                 in_term2 = Jnpp_r_alpha_mn * Jnpp_r0_alpha_mn;
@@ -293,27 +315,32 @@ namespace greens_functions
                 }
             }
             if(m == num_in_term_use)
-                std::cout << "warning: use term over num_in_term_use" << std::endl;
+                std::cout << "warning: too slow convergence in p_int_theta_2nd m."
+                          << std::endl;
 
-            term = in_sum * sin(bessel_order * theta) * cos(n * M_PI / 2e0) / bessel_order;
+            term = sgn * in_sum * sin(bessel_order * theta) / n;
             sum += term;
+
+            sgn *= -1e0;
 
 //             std::cout << "outer sum " << sum << ", term" << term << std::endl;
 
-            if(fabs(in_sum / (bessel_order * sum)) < threshold)
+            if(fabs(in_sum / (n * sum)) < threshold)
             {
-                /* if bessel_order * theta = product of natural number and pi, *
-                 * term become zero and this code breaks too early. Therefore, *
-                 * consider in_sum / bessel_order only here.                   */
-
 //                 std::cout << "normal exit. n = " << n << " second term" << std::endl;
                 break;
+                /* if bessel_order * theta = product of natural number and pi,*
+                 * term become zero and this code breaks loop too early.      *
+                 * Therefore, consider the difference between sum and         *
+                 * in_sum / n only. sin is always in range(-1,1). the effect  *
+                 * is small.                                                  */
             }
         }
         if(n == num_out_term_use)
-            std::cout << "warning: use term over num_out_term_use" << std::endl;
+            std::cout << "warning: too slow convergence in p_int_theta_2nd n."
+                      << std::endl;
 
-        return (16e0 * sum / (this->phi * a * a));
+        return (8e0 * sum / (M_PI * a * a));
     }
 
     const Real GreensFunction2DRefWedgeAbs::p_int_phi(const Real r, const Real t) const
@@ -518,19 +545,23 @@ namespace greens_functions
         THROW_UNLESS(std::invalid_argument, 0.0<=rnd && rnd <= 1.0);
 
         if(rnd == 1e0)
-            return 2e0 * M_PI;
+            return phi;
 
         if(fabs(r) < CUTOFF)// r == 0e0 ?
         {
             throw std::invalid_argument(
-                    (boost::format("2DAbs::drawTheta r is too small: r=%f10") % r).str());
+                    (boost::format(
+                         "2DRefWedgeAbs::drawTheta r is too small: r=%f10"
+                         ) % r).str());
         }
 
         if(fabs(r-a) < CUTOFF)// r == a ?
         {
             //when R equal a, p_int_theta is zero at any theta
             throw std::invalid_argument(
-                    (boost::format("2DAbs::drawTheta r is nealy a: r=%f10, a=%f10") % r % a).str());
+                    (boost::format(
+                         "2DAbs::drawTheta r is nealy a: r=%f10, a=%f10"
+                         ) % r % a).str());
         }
 
         if(t == 0e0 || D == 0e0)
