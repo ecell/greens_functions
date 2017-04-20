78bb19ff03645c7aea0163af72919800c992334a
Kazunari Kaizu <kwaizu@gmail.com>
2017-04-20 20:22:35 +0900

    config.h is only meaningful in compat.h

--- GreensFunction3D.cpp
+++ GreensFunction3D.cpp
@@ -1,7 +1,3 @@
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif /* HAVE_CONFIG_H */
-
 #include <sstream>
 #include <boost/format.hpp>
 #include <gsl/gsl_errno.h>
--- GreensFunction3DRadAbsBase.cpp
+++ GreensFunction3DRadAbsBase.cpp
@@ -1,7 +1,3 @@
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#endif
-
 #include "GreensFunction3DRadAbsBase.hpp"
 
 namespace greens_functions
