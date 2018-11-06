5d67ad7b977d24b0bd8830db4593ca4f5c504cd6
ToruNiina <niina.toru.68u@gmail.com>
2018-11-06 16:28:33 +0900

    strip extraneous whitespace

--- funcSum.cpp
+++ funcSum.cpp
@@ -13,7 +13,7 @@ typedef std::vector<Real> RealVector;
 
 //static Logger& _log(Logger::get_logger("funcSum"));
 
-Real 
+Real
 funcSum_all(boost::function<Real(unsigned int i)> f, size_t max_i)
 {
     Real sum(0.0);
@@ -26,7 +26,7 @@ funcSum_all(boost::function<Real(unsigned int i)> f, size_t max_i)
 
     sum = p_0;
 
-    RealVector::size_type i(1); 
+    RealVector::size_type i(1);
     while(i < max_i)
     {
         const Real p_i(f(i));
@@ -39,7 +39,7 @@ funcSum_all(boost::function<Real(unsigned int i)> f, size_t max_i)
 }
 
 
-Real 
+Real
 funcSum_all_accel(boost::function<Real(unsigned int i)> f,
                   size_t max_i, Real tolerance)
 {
@@ -63,9 +63,9 @@ funcSum_all_accel(boost::function<Real(unsigned int i)> f,
 
     Real sum;
     Real error;
-    gsl_sum_levin_utrunc_workspace* 
+    gsl_sum_levin_utrunc_workspace*
         workspace(gsl_sum_levin_utrunc_alloc(i));
-    gsl_sum_levin_utrunc_accel(&pTable[0], pTable.size(), workspace, 
+    gsl_sum_levin_utrunc_accel(&pTable[0], pTable.size(), workspace,
                                 &sum, &error);
     if (fabs(error) >= fabs(sum * tolerance))
     {
@@ -82,29 +82,29 @@ funcSum_all_accel(boost::function<Real(unsigned int i)> f,
 }
 
 
-Real 
+Real
 funcSum(boost::function<Real(unsigned int i)> f, size_t max_i, Real tolerance)
 // funcSum
 // ==
 // Will simply calculate the sum over a certain function f, until it converges
-// (i.e. the sum > tolerance*current_term for a CONVERGENCE_CHECK number of 
+// (i.e. the sum > tolerance*current_term for a CONVERGENCE_CHECK number of
 // terms), or a maximum number of terms is summed (usually 2000).
 //
 // Input:
 // - f: A       function object
 // - max_i:     maximum number of terms it will evaluate
 // - tolerance: convergence condition, default value 1-e8 (see .hpp)
-// 
+//
 // About Boost::function
 // ===
-// Boost::function doesn't do any type checking: It will take any object 
-// and any signature you provide in its template parameter, and create an 
-// object that's callable according to your signature and calls the object. 
+// Boost::function doesn't do any type checking: It will take any object
+// and any signature you provide in its template parameter, and create an
+// object that's callable according to your signature and calls the object.
 // If that's impossible, it's a compile error.
 // (From: http://stackoverflow.com/questions/527413/how-boostfunction-and-boostbind-work)
 {
     // DEFAULT = 4
-    const unsigned int CONVERGENCE_CHECK(4);    
+    const unsigned int CONVERGENCE_CHECK(4);
 
     Real sum(0.0);
     RealVector pTable;
@@ -122,7 +122,7 @@ funcSum(boost::function<Real(unsigned int i)> f, size_t max_i, Real tolerance)
 
     unsigned int convergenceCounter(0);
 
-    RealVector::size_type i(1); 
+    RealVector::size_type i(1);
     while(i < max_i)
     {
         const Real p_i(f(i));
@@ -133,7 +133,7 @@ funcSum(boost::function<Real(unsigned int i)> f, size_t max_i, Real tolerance)
 
         if (fabs(sum) * tolerance >= fabs(p_i)) // '=' is important
         {
-            ++convergenceCounter;          
+            ++convergenceCounter;
         }
         // this screws it up; why?
         else
@@ -147,15 +147,15 @@ funcSum(boost::function<Real(unsigned int i)> f, size_t max_i, Real tolerance)
             extrapolationNeeded = false;
             break;
         }
-        
+
     }
 
     if (extrapolationNeeded)
     {
         Real error;
-        gsl_sum_levin_utrunc_workspace* 
+        gsl_sum_levin_utrunc_workspace*
             workspace(gsl_sum_levin_utrunc_alloc(i));
-        gsl_sum_levin_utrunc_accel(&pTable[0], pTable.size(), workspace, 
+        gsl_sum_levin_utrunc_accel(&pTable[0], pTable.size(), workspace,
             &sum, &error);
         if (fabs(error) >= fabs(sum * tolerance * 10))
         {
