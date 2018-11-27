d60fcaee1a2a108c92e464ed28f1c9e7429c0dbd
ToruNiina <niina.toru.68u@gmail.com>
2018-11-27 15:41:12 +0900

    use funcSum if max_i is enough small in p_survival
    `funcSum_all` just sums up all the terms in [0, max_i) without GSL
    series acceleration algorithm. `funcSum` uses series acceleration
    if the series does not pass the convergence check. In general, to
    obtain a sum of a series, series acceleration algorithm is useful.
    But using just `funcSum` causes NaN values because some series may
    contains really small (denormalized) numbers and really small max_i.
    To avoid these error, now the code uses funcSum if max_i is enough
    large. This prevents crash in some (extreme) conditions, and may
    improve the accuracy of the result a bit.

--- GreensFunction2DRadAbs.cpp
+++ GreensFunction2DRadAbs.cpp
@@ -857,18 +857,30 @@ GreensFunction2DRadAbs::p_survival_table( const        Real t,
                 "couldn't converge; max terms reached: %1%\n") % maxi;
     }
 
-    if( psurvTable.size() < maxi + 1 )           // if the dimensions are good then this means
-    {                                            // that the table is filled
-        getAlpha( 0, maxi );                      // this updates the table of roots
-        this->createPsurvTable( psurvTable);      // then the table is filled with data
+    if( psurvTable.size() < maxi + 1 )       // if the dimensions are good then this means
+    {                                        // that the table is filled
+        getAlpha( 0, maxi );                 // this updates the table of roots
+        this->createPsurvTable( psurvTable); // then the table is filled with data
     }
-        // A sum over terms is performed, where convergence is assumed. It is not
+
+    // A sum over terms is performed, where convergence is assumed. It is not
     // clear if this is a just assumption.
-    // TODO!
-    p = funcSum_all( boost::bind( &GreensFunction2DRadAbs::p_survival_i_exp_table,
-                                  this,
-                                  _1, t, psurvTable ),
-                                  maxi ); // calculate the sum at time t
+    if(maxi < 8) //TODO consider this threshold
+    {
+        p = funcSum_all(boost::bind(
+                    &GreensFunction2DRadAbs::p_survival_i_exp_table,
+                    this, _1, t, psurvTable
+                ), maxi); // calculate the sum at time t
+    }
+    else
+    {
+        p = funcSum(boost::bind(
+                    &GreensFunction2DRadAbs::p_survival_i_exp_table,
+                    this, _1, t, psurvTable
+                ), maxi); // calculate the sum at time t
+    }
+    assert(std::isfinite(p));
+
     return p*M_PI*M_PI_2;
 }
 
