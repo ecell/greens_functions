1d9b713f2810b618e3920a8fcece263e559263b7
ToruNiina <niina.toru.68u@gmail.com>
2019-06-18 11:43:30 +0900

    :checkered_flag: undef min/max, stop define min/max

--- compat.h
+++ compat.h
@@ -71,4 +71,14 @@ inline bool isfinite(long double x)
 } // greens_functions
 #endif // ISFINITE
 
+#ifdef _MSC_BUILD
+#ifdef min
+#undef min
+#endif
+#ifdef max
+#undef max
+#endif
+#define NOMINMAX
+#endif // MSVC & Windows
+
 #endif // ECELL_GREENS_FUNCTIONS_COMPAT_HPP
