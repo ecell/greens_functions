b00a1ef068963201448612d7f0608372e4e7c8c1
ToruNiina <niina.toru.68u@gmail.com>
2019-07-02 17:14:45 +0900

    :art: remove extraneous whitespaces

--- GreensFunction3DRadInf.hpp
+++ GreensFunction3DRadInf.hpp
@@ -5,10 +5,10 @@
 #include <vector>
 #include <gsl/gsl_integration.h>
 
-//#include "Logger.hpp"
 #include "PairGreensFunction.hpp"
 
-namespace greens_functions{
+namespace greens_functions
+{
 
 class GreensFunction3DRadInf: public PairGreensFunction
 {
@@ -32,33 +32,33 @@ private:
     static const unsigned int MAX_ORDER = 70;
 
     static const Real H;
-    
+
 public:
-    
+
     GreensFunction3DRadInf(Real D, Real kf, Real r0, Real Sigma);
 
     virtual ~GreensFunction3DRadInf();
- 
+
     virtual Real drawTime(Real rnd) const;
-    
+
     virtual Real drawR(Real rnd, Real t) const;
-    
+
     virtual Real drawTheta(Real rnd, Real r, Real t) const;
-    
+
     Real getkD() const
     {
         return this->kD;
     }
-    
+
     Real getalpha() const
     {
         return this->alpha;
     }
-    
+
     Real p_reaction(Real t) const;
     Real p_survival(Real t) const;
     Real p_int_r(Real r, Real t) const;
-    
+
     Real p_theta(Real theta, Real r, Real time) const;
 
     Real ip_theta(Real theta, Real r, Real time) const;
@@ -66,7 +66,7 @@ public:
     Real p_free(Real theta, Real r, Real t) const;
 
     Real ip_free(Real theta, Real r, Real t) const;
-    
+
     Real p_corr(Real theta, Real r, Real t) const;
 
     Real ip_corr(Real theta, Real r, Real t) const;
@@ -81,25 +81,25 @@ public:
 private:
     Real p_corr_R(Real alpha, unsigned int n, Real r, Real t) const;
 
-    
+
     Real p_corr_n(unsigned int n, RealVector const& RnTable, RealVector const& lgndTable) const;
 
     Real ip_corr_n(unsigned int n, RealVector const& RnTable, RealVector const& lgndTable) const;
 
     Real p_corr_table(Real theta, Real r, Real t, RealVector const& RnTable) const;
 
-    Real 
+    Real
     ip_corr_table(Real theta, Real r, Real t, RealVector const& RnTable) const;
-    
+
     Real p_theta_table(Real r, Real theta, Real time,
                        RealVector const& RnTable) const;
 
     Real ip_theta_table(Real r, Real theta, Real time,
                         RealVector const& RnTable) const;
 
-    Real 
+    Real
     p_corr_table(Real theta, Real r, Real t, RealVector const& RnTable);
-    
+
 
     void makeRnTable(RealVector& RnTable, Real r, Real t) const;
 
@@ -109,11 +109,11 @@ private:
 private:
     static Real p_corr_R_F(Real, p_corr_R_params*);
     static Real ip_theta_F(Real theta, p_theta_params* params);
-    
+
 private:
     const Real kD;
     const Real alpha;
-   
+
 //    static Logger& log_;
 };
 
